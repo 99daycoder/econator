296929b5f3499bf58df2c2dda0529b2121022211
m4tchka <91279022+m4tchka@users.noreply.github.com>
2022-08-17 12:13:44 +0100

    x

--- src/Components/PlasticUse.js
+++ src/Components/PlasticUse.js
@@ -1,7 +1,27 @@
-import React from 'react'
+import React from "react";
+import { FaBeer, MdOutlineFastfood, MdOutlineNoFood } from "react-icons/fa";
+import { logo } from "../Images/econator.png";
+
+class Question extends React.Component {
+    render() {
+        return (
+            <>
+                {" "}
+                <FaRegUser />{" "}
+            </>
+        );
+    }
+}
 
 export default function PlasticUse() {
-  return (
-    <div>PlasticUse</div>
-  )
+    return (
+        <div>
+            PlasticUse <img src={logo} />
+            <FaRegUser />
+            <div>
+                <p>Did you use single use plastic today ?</p>
+                {/* <MdOutlineFastfood /> <MdOutlineNoFood /> */}
+            </div>
+        </div>
+    );
 }
