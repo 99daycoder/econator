7f900a119dc14b72a5b030e81fc9dd67d661d1f3
Phantom <sambaeverything@gmail.com>
2022-08-17 15:25:31 +0200

    Created props on App.js

--- src/App.js
+++ src/App.js
@@ -6,28 +6,23 @@ import { useState } from "react";
 import "./App.css";
 import Login from "./Components/Login";
 import { ChakraProvider } from "@chakra-ui/react";
-
-
 import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
 import Profile from './Components/Profile.js'
+import Results from "./Components/Results";
 
 function App() {
   const [question1, setQuestion1] = useState("");
   const [question2, setQuestion2] = useState("");
-  let displayedQuestion;
-  if (!question1) {
-    displayedQuestion = <PlasticUse setQuestion1={setQuestion1} />;
-  } else if (question1 && !question2) {
-    displayedQuestion = <Transportation setQuestion2={setQuestion2} />;
-  }
-
+  const [results, setResults] = useState("")
+ 
   return (
     <ChakraProvider>
        <Routes>
         <Route path="/" element={<Login />} />
-        <Route path="plastic" element={<PlasticUse />} />
-        <Route path="profile" element={<Profile />} />
-        <Route path="trans" element={<Transportation />} />
+        <Route path="plastic" element={<PlasticUse setQuestion1={setQuestion1} question1={question1}/>} />
+        <Route path="profile" element={<Profile  /> } />
+        <Route path="transportation" element={<Transportation question2={question2} setQuestion2={setQuestion2} />} />
+        <Route path="results" element={<Results results={results} setResults={setResults} />} />
       </Routes>
     </ChakraProvider>
   );
