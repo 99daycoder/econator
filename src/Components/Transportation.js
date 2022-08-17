45dd6872d9e04490603b5ddb320e2271b2274115
nafisoaden97 <nafisoaden97@gmail.com>
2022-08-17 14:39:31 +0100

    y

--- src/Components/Transportation.js
+++ src/Components/Transportation.js
@@ -1,7 +1,30 @@
-import React from 'react'
+import React from "react";
+import {
+  FaUserAlt,
+  AiFillCar,
+  BsBicycle,
+  FaBusAlt,
+  FaWalking,
+} from "react-icons/fa";
+import logo from "../Images/econator.png";
 
-export default function Transportation() {
-  return (
-    <div>Transportation</div>
-  )
+export default class Transportation extends React.Component {
+  render() {
+    function handleClick() {
+      this.props.setQuestion1("plastic");
+    }
+    return (
+      <>
+        <div>
+          <img className="logo" src={logo} alt="econantor" />
+          <FaUserAlt className="userIcon" />
+          <p className="text">Did you use single use plastic today ?</p>
+          <AiFillCar />
+          <FaWalking />
+          <FaBusAlt />
+          <BsBicycle />
+        </div>
+      </>
+    );
+  }
 }
