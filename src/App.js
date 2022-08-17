1230435e6460f29e97da8e4a1b67e55980d00588
Phantom <sambaeverything@gmail.com>
2022-08-17 15:20:56 +0200

    added react router

--- src/App.js
+++ src/App.js
@@ -1,46 +1,36 @@
-
 import logo from "./logo.svg";
 import "./App.css";
 import PlasticUse from "./Components/PlasticUse.js";
 import Transportation from "./Components/Transportation.js";
 import { useState } from "react";
-import './App.css';
-import Login from './Components/Login';
-import { ChakraProvider } from '@chakra-ui/react'
+import "./App.css";
+import Login from "./Components/Login";
+import { ChakraProvider } from "@chakra-ui/react";
+
+
+import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
+import Profile from './Components/Profile.js'
 
 function App() {
-    const [question1, setQuestion1] = useState("");
-    const [question2, setQuestion2] = useState("");
-    let displayedQuestion;
-    if (!question1) {
-        displayedQuestion = <PlasticUse setQuestion1={setQuestion1} />;
-    } else if (question1 && !question2) {
-        displayedQuestion = <Transportation setQuestion2={setQuestion2}/>
-    }
+  const [question1, setQuestion1] = useState("");
+  const [question2, setQuestion2] = useState("");
+  let displayedQuestion;
+  if (!question1) {
+    displayedQuestion = <PlasticUse setQuestion1={setQuestion1} />;
+  } else if (question1 && !question2) {
+    displayedQuestion = <Transportation setQuestion2={setQuestion2} />;
+  }
 
-
-
-        return (
-            <div className="App">
-                {displayedQuestion}
-            </div>
-        );
-
-
-
-
-
-
-function App() {
   return (
     <ChakraProvider>
-    <div>
-    
-<Login/> 
-    </div>
+       <Routes>
+        <Route path="/" element={<Login />} />
+        <Route path="plastic" element={<PlasticUse />} />
+        <Route path="profile" element={<Profile />} />
+        <Route path="trans" element={<Transportation />} />
+      </Routes>
     </ChakraProvider>
   );
-
 }
 
 export default App;
--- src/index.js
+++ src/index.js
@@ -4,6 +4,8 @@ import './index.css';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
 import { Auth0Provider } from "@auth0/auth0-react";
+import { BrowserRouter } from "react-router-dom";
+
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
@@ -13,7 +15,9 @@ root.render(
     redirectUri={window.location.origin}
   >
   <React.StrictMode>
+  <BrowserRouter>
     <App />
+    </BrowserRouter>
   </React.StrictMode>
   </Auth0Provider>
 );
